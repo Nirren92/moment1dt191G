e4f687c6422fa666c05473e8e62edfef37af61e7
Nirren92 <143922852+Nirren92@users.noreply.github.com>
2025-02-02 22:29:22 +0100

    klart för idag

--- obj/Debug/net9.0/moment1.AssemblyInfo.cs
+++ obj/Debug/net9.0/moment1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("moment1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3066bcd0482d4781c01bd0c7e75460faf4e91af7")]
 [assembly: System.Reflection.AssemblyProductAttribute("moment1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("moment1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
